1299b2b1970e8e132f47c723fbfbc5e7d00ce94f
20042000 <karol.ladino@mail.escuelaing.edu.co>
2024-03-20 20:37:09 -0500

    Formulario login y mensajes

--- secureLogin/src/main/resources/public/index.js
+++ secureLogin/src/main/resources/public/index.js
@@ -1,8 +1,32 @@
 function cadenaLog(){
     let inputUserName = document.getElementById("username");
-    let inputContrasena = document.getElementById("contrasena");
+    let inputPassword = document.getElementById("password");
     let endPoint = "/login";
-    let peticion = solicitarEndPoint(endPoint, body);
+    let peticion = solicitarEndPoint(endPoint, {
+        username: inputUserName.value,
+        password: inputPassword.value
+    });
+    peticion.then(respuesta =>{
+        let divResLogin = document.getElementById("resLogin");
+        
+        let resp = new Boolean(respuesta);
+        divResLogin.innerHTML = (resp)?"Bienvenido 😊 " + inputUserName.value :"Usuario o contraseña incorrectos 😔";
+    })
 }
 
-async function solicitarEndPoint(endPoint, body){}
+async function solicitarEndPoint(endPoint, body){
+    return await fetch(window.location.origin + endPoint, {
+        headers: {
+            "Content-Type": "application/json"
+        },
+        method: "POST",
+        body: body
+    })
+    .then(respuesta => {
+        if(!respuesta.ok){
+            throw new Error();
+        }
+        return respuesta.text();
+    })
+    .catch(error => alert(error));
+}
